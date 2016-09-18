4e670b1099d94fd3e72650a0af21eb7f78d16ee3
johno <johno>
2016-09-18 14:48:58 -0400

    working local hello world

--- src/main/java/com/johnpoulakos/TextingApplication.java
+++ src/main/java/com/johnpoulakos/TextingApplication.java
@@ -2,7 +2,9 @@ package com.johnpoulakos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = { "com.johnpoulakos.controllers"})
 @SpringBootApplication
 public class TextingApplication {
 
